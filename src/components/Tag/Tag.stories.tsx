51323fd9e260e6b7e5c5cb9bf639917b140a078b
Quan Truong <quan@devteamsix,com>
2022-02-21 08:07:36 -0700

    First commit

---
+++ src/components/Tag/Tag.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Tag from './index'
+
+// This just shows that it can be defined differently. This is the 
+// default export
+export default {
+     title: 'Tag title',
+     component: Tag
+}
+export const Basic = () => (
+     <Tag />
+)
+export const Second = () => (
+     <Tag title="My second tag" />
+)
